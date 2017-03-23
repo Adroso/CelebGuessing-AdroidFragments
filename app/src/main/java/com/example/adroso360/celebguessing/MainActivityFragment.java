21da88d487882a59ed219eb3f60606388a27ce27
Adroso360 <adrian.lapico@my.jcu.edu.au>
2017-03-23 17:07:43 +1000

    guessing logic is now good, added grid view dynamics

--- app/src/main/java/com/example/adroso360/celebguessing/MainActivityFragment.java
+++ app/src/main/java/com/example/adroso360/celebguessing/MainActivityFragment.java
@@ -1,6 +1,6 @@
 package com.example.adroso360.celebguessing;
 
-import android.content.Context;
+
 import android.content.res.TypedArray;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -17,6 +17,7 @@ import java.util.Objects;
 
 import static android.R.layout.simple_list_item_1;
 
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -25,46 +26,64 @@ public class MainActivityFragment extends Fragment {
     private GridView gridview;
     private ArrayAdapter<String> names;
     private ImageView imgView;
+    public int currentPicture;
+    private int gridSum = 3;
 
     public MainActivityFragment() {
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_main, container, false);
-
         imgView = (ImageView) v.findViewById(R.id.imgView);
-
-        final TypedArray images = getResources().obtainTypedArray(R.array.celebImages);
-        final int choice = (int) (Math.random() * images.length());
-        final int currentPicture = images.getIndex(choice);
-        imgView.setImageResource(images.getResourceId(choice, R.drawable.adele)); //adele on default
-        images.recycle();
-
-        //Obama ID:23
-        //Trump ID:24
-        //Adele ID:3
-        //Hammo ID:9
-        //Cordo ID:21
-        //Downy ID:4
-
-
-
+        randomImage();
+        try {
+            gridSum = SecondaryFragment.gridAmount;
+        } catch (Exception e){
+            gridSum = 3;
+        }
         names = new ArrayAdapter<String>(v.getContext(), simple_list_item_1);
         gridview = (GridView) v.findViewById(R.id.gridView);
         gridview.setAdapter(names);
+        names.clear();
+        if (gridSum==3){
+            names.add("Adele");
+            names.add("James Cordon");
+            names.add("Robert Downey Jr");
+        }else if(gridSum == 5){
+            names.add("Adele");
+            names.add("James Cordon");
+            names.add("Robert Downey Jr");
+            names.add("Richard Hammond");
+            names.add("Obama");
+        }else if(gridSum == 8){
+            names.add("Adele");
+            names.add("James Cordon");
+            names.add("Robert Downey Jr");
+            names.add("Richard Hammond");
+            names.add("Obama");
+            names.add("Trump");
+            names.add("Homer Simpson");
+            names.add("Jason Holdsworth");
+            names.add("Adam");
+        }
 
-        names.add("Adele");
-        names.add("James Cordon");
-        names.add("Robert Downey Jr");
-        names.add("Richard Hammond");
-        names.add("Obama");
-        names.add("Trump");
-        names.add("Homer Simpson");
-        names.add("Jason Holdsworth");
-        names.add("Adam");
+//        names = new ArrayAdapter<String>(v.getContext(), simple_list_item_1);
+//        gridview = (GridView) v.findViewById(R.id.gridView);
+//        gridview.setAdapter(names);
+//
+//
+//        names.add("Adele");
+//        names.add("James Cordon");
+//        names.add("Robert Downey Jr");
+//        names.add("Richard Hammond");
+//        names.add("Obama");
+//        names.add("Trump");
+//        names.add("Homer Simpson");
+//        names.add("Jason Holdsworth");
+//        names.add("Adam");
+
 
 
 
@@ -72,10 +91,10 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(v.getContext(),names.getItem(position), Toast.LENGTH_SHORT).show();
-                System.out.println();
                Boolean playerWin = checkWin(names.getItem(position), currentPicture);
                 if (playerWin){
                     Toast.makeText(v.getContext(),"YOU WON", Toast.LENGTH_SHORT).show();
+                    randomImage();
                 }else {
                     Toast.makeText(v.getContext(),"TRY AGAIN", Toast.LENGTH_SHORT).show();
                 }
@@ -87,14 +106,16 @@ public class MainActivityFragment extends Fragment {
     }
 
     public static boolean checkWin(String userSelection, int currentPic){
-        //Obama ID:23
-        //Trump ID:24
-        //Adele ID:3
-        //Hammo ID:9
-        //Cordo ID:21
-        //Downy ID:4
+        //Obama ID:23 Trump ID:24 Adele ID:3 Hammo ID:9 Cordo ID:21 Downy ID:4
+        return (Objects.equals(userSelection, "Adele") && currentPic == 0) || (Objects.equals(userSelection, "James Cordon") && currentPic == 1) || (Objects.equals(userSelection, "Robert Downey Jr") && currentPic == 2) || (Objects.equals(userSelection, "Richard Hammond") && currentPic == 3) || (Objects.equals(userSelection, "Obama") && currentPic == 4) || (Objects.equals(userSelection, "Trump") && currentPic == 5) ;
 
-        return (Objects.equals(userSelection, "Adele") && currentPic == 3) || (Objects.equals(userSelection, "James Cordon") && currentPic == 21) || (Objects.equals(userSelection, "Robert Downer Jr") && currentPic == 4) || (Objects.equals(userSelection, "Richard Hammond") && currentPic == 9) || (Objects.equals(userSelection, "Obama") && currentPic == 23) || (Objects.equals(userSelection, "Trump") && currentPic == 24);
-
+    }
+    public void randomImage(){
+        final TypedArray images = getResources().obtainTypedArray(R.array.celebImages);
+        final int choice = (int) (Math.random() * images.length());
+        currentPicture = choice;
+        //System.out.println(choice);
+        imgView.setImageResource(images.getResourceId(choice, R.drawable.adele)); //adele on default
+        images.recycle();
     }
 }
--- app/src/main/java/com/example/adroso360/celebguessing/SecondaryFragment.java
+++ app/src/main/java/com/example/adroso360/celebguessing/SecondaryFragment.java
@@ -6,6 +6,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -13,17 +19,51 @@ import android.view.ViewGroup;
  */
 public class SecondaryFragment extends Fragment {
 
+    public Button triGrid;
+    public Button pentaGrid;
+    public Button octaGrid;
+    public static int gridAmount;
 
     public SecondaryFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_secondary, container, false);
+        View v2 = inflater.inflate(R.layout.fragment_secondary, container, false);
+        triGrid = (Button) v2.findViewById(R.id.button);
+        pentaGrid = (Button) v2.findViewById(R.id.button2);
+        octaGrid = (Button) v2.findViewById(R.id.button3);
+
+        triGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gridAmount = 3;
+            }
+        });
+        pentaGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gridAmount = 5;
+
+            }
+        });
+        octaGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gridAmount = 8;
+
+            }
+        });
+
+
+
+        return v2;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
